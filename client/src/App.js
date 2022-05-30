0859fd9ee636d2b0e1729868e29a847bceb9185e
chuae95 <chua.e95@gmail.com>
2022-05-30 23:34:51 +0800

    testing

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+
+function App() {
+
+  
+
+  // useEffect(() => {
+  //   fetch("/api")
+  //     .then(resp => {
+  //       resp.json();
+  //     }) 
+  //     .then(info => {
+  //       setData(info);
+  //     }
+  //   )
+  // }, [])
+
+  return (
+    <div>
+      Main Page testing
+    </div>
+  )
+
+}
+
+export default App;
